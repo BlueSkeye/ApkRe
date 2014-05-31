fc7b60bd7f07b41b75f772f5053b9be4fba2d28d
Blue Skeye <bsgithub@gmail.com>
2014-05-31 16:32:45 +0200

    Added Zip decompression support and APK content extraction

---
+++ ApkHandler/Zip/EndOfCentralDirectory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.rackham.ApkHandler.Zip
+{
+    internal class EndOfCentralDirectory
+    {
+        internal EndOfCentralDirectory(byte[] buffer, int offset)
+        {
+            int remainingBytes = buffer.Length - offset;
+            if (remainingBytes < ZipExtractor.MinimumEOCDLength) {
+                throw new ZipFormatException("Not enough data for an EOCD");
+            }
+            if ((0x50 != buffer[offset++])
+                || (0x4B != buffer[offset++])
+                || (0x05 != buffer[offset++])
+                || (0x06 != buffer[offset++]))
+            {
+                throw new ZipFormatException("EOCD signature missing");
+            }
+            DiskNumber = Helpers.ReadUInt16(buffer, ref offset);
+            if (0 != DiskNumber) {
+                throw new ZipNotSupportedFormatException("Multi disk format.");
+            }
+            EOCDDiskNumber = Helpers.ReadUInt16(buffer, ref offset);
+            if (EOCDDiskNumber != DiskNumber) {
+                throw new ZipNotSupportedFormatException("Multi disk format.");
+            }
+            ThisDiskRecordsCount = Helpers.ReadUInt16(buffer, ref offset);
+            TotalRecordsCount = Helpers.ReadUInt16(buffer, ref offset);
+            if (TotalRecordsCount != ThisDiskRecordsCount) {
+                throw new ZipNotSupportedFormatException("Multi disk format.");
+            }
+            CentralDirectorySize = Helpers.ReadUInt32(buffer, ref offset);
+            CentralDirectoryStartOffset = Helpers.ReadUInt32(buffer, ref offset);
+            ushort commentLength = Helpers.ReadUInt16(buffer, ref offset);
+            remainingBytes = buffer.Length - offset;
+            if (remainingBytes < commentLength) {
+                throw new ZipFormatException("Not enough data for an EOCD");
+            }
+            Comment = UTF8Encoding.UTF8.GetString(buffer, offset, commentLength);
+            return;
+        }
+
+        #region PROPERTIES
+        internal uint CentralDirectorySize { get; private set; }
+
+        internal uint CentralDirectoryStartOffset { get; private set; }
+
+        internal string Comment { get; private set; }
+
+        internal ushort DiskNumber { get; private set; }
+
+        internal ushort EOCDDiskNumber { get; private set; }
+
+        internal ushort ThisDiskRecordsCount { get; private set; }
+
+        internal ushort TotalRecordsCount { get; private set; }
+        #endregion
+    }
+}
---
+++ ApkHandler/Zip/LocalFileHeader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace com.rackham.ApkHandler.Zip
+{
+    internal class LocalFileHeader : FileHeader
+    {
+        #region CONSTRUCTORS
+        internal LocalFileHeader(FileStream input)
+            : base(input)
+        {
+            byte[] buffer = new byte[base.FileNameLength + base.ExtraFieldLength];
+            if (buffer.Length != input.Read(buffer, 0, buffer.Length)) {
+                throw new ZipFormatException("Not enough data for LFH.");
+            }
+            int offset = 0;
+            base.ReadFileNameAndExtraField(buffer, ref offset);
+            return;
+        }
+        #endregion
+
+        #region PROPERTIES
+        protected override byte[] ExpectedSignature
+        {
+            get { return (byte[])_signatureBytes.Clone(); }
+        }
+
+        protected override int MinimumFileHeaderSize
+        {
+            get { return 30; }
+        }
+        #endregion
+
+        #region FIELDS
+        private static readonly byte[] _signatureBytes = new byte[] { 0x50, 0x4B, 0x03, 0x04 };
+        #endregion
+    }
+}
